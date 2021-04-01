4d79b8d7915a25b4df3005f571dc0b23385ba0ab
Md Ashfaqur Rahman <ashfaq.iosdev@gmail.com>
2021-04-01 23:08:17 +0600

    Initial Commit

---
+++ Talentttorrent/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Talentttorrent
+//
+//  Created by Md Ashfaqur Rahman on 1/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
